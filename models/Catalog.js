12dc6f4bae569c297161ec6dc47a88aa8620356c
arefesalehi <41992279+arefesalehi@users.noreply.github.com>
2026-05-24 12:22:15 +0330

    chore: optimize .gitignore to reduce source size for Liara deployment

--- models/Catalog.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const CatalogSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  file: { type: String, required: true },
-});
-
-export default mongoose.models.Catalog || mongoose.model('Catalog', CatalogSchema);
